0cf01341b0857e4f218f71aa70626e46e1592d73
Matt Cole <mcole.uk@gmail.com>
2025-02-18 20:12:22 +1030

    seperating the railmon app

--- apps/railmon/src/axios/common-database.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:1338/cd/api/0.1',
-    timeout: 1000
-});
-
-export default instance;
--- apps/railmon/src/axios/spark.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://metspark.co.uk/spark/api/0.1',
-    timeout: 3000
-});
-
-export default instance;
